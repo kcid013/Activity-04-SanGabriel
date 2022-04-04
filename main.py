9c08430685418ad77ad290a059835b2567ff0cf0
Dick <kcid013@gmail.com>
2022-04-04 21:08:01 +0800

    updated 4/4/22

--- main.py
+++ main.py
@@ -1,9 +1,37 @@
 import cv2
 
-filePath = 'cat.jpg'
-windowTitle = 'picture of cat'
-readCVimg = cv2.imread(filePath, -1)
+print ("""[1] - fixed image of a cat
+[2] - select your own image and flag
+""")
 
-cv2.imshow(windowTitle, readCVimg)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+opt = int(input("select: "))
+
+if opt == 1:
+    filePath = 'cat.jpg'
+    windowTitle = 'picture of cat'
+    readCVimg = cv2.imread(filePath, -1)
+    cv2.imshow(windowTitle, readCVimg)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+elif opt == 2:
+    windowTitle = 'chosen picture'
+    filePath = input("type the name and extension of the image: ")
+    print("""type of flags:
+    [1] - colored
+    [2] - grayscale
+    [3] - unchanged
+    """)
+    opt = int(input("select: "))
+
+    if opt == 1:
+        flagType = 1
+    elif opt == 2:
+        flagType = 0
+    elif opt == 3:
+        flagType = -1
+
+    readCVimg = cv2.imread(filePath, flagType)
+    cv2.imshow(windowTitle, readCVimg)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
